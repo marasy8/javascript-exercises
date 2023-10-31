536266d863e597d9490bab81da65f78e7bc01b9b
Charles <zai@ualberta.ca>
2023-10-30 19:50:03 -0600

    until exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...data) {
+    let output = [];
+    if (data.length < 2) {
+        if (data in array) {
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] != data) {
+                    output.push(array[i]);
+                }
+            }
+            return output;
+        } else {
+            return array;
+        }
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (!(data.includes(array[i]))) {
+                output.push(array[i]);
+            }
+        }
+        console.log(array[3] in data);
+        console.log("data " + data);
+        console.log("array " + array);
+        console.log("output " + output);
+        return output;
+    }
+    
+    
 };
 
 // Do not edit below this line
